3ec45ce1b5bb9ed7450bd4cdeb86d153d17166d7
rahul <rahul.biswas@uprise.co>
2021-12-23 00:16:58 +0530

    added create table query

--- employees.sql
+++ employees.sql
@@ -20,13 +20,27 @@ SET row_security = off;
 -- Data for Name: employees; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-INSERT INTO public.employees (id, name, "position", rehire_eligible, voluntary, terminated_date, termination_reason, profile_link, email, "createdAt", "updatedAt") VALUES (0, 'Tim', 'Senior Software Engineer', true, true, '23 March 2021', NULL, 'https://linkedin.com', 'tim@gmail.com', '2021-12-21 21:48:17.657+05:30', '2021-12-21 21:48:17.657+05:30');
-INSERT INTO public.employees (id, name, "position", rehire_eligible, voluntary, terminated_date, termination_reason, profile_link, email, "createdAt", "updatedAt") VALUES (1, 'Bob', 'Senior Software Engineer', true, false, '22 March 2021', NULL, 'https://linkedin.com', 'bob@gmail.com', '2021-12-21 21:48:17.657+05:30', '2021-12-21 21:48:17.657+05:30');
-INSERT INTO public.employees (id, name, "position", rehire_eligible, voluntary, terminated_date, termination_reason, profile_link, email, "createdAt", "updatedAt") VALUES (2, 'Susan', 'Senior Software Engineer', true, true, '22 March 2021', 'Left because of a new opportunity', 'https://linkedin.com', 'susan@gmail.com', '2021-12-21 21:48:17.657+05:30', '2021-12-21 21:48:17.657+05:30');
-INSERT INTO public.employees (id, name, "position", rehire_eligible, voluntary, terminated_date, termination_reason, profile_link, email, "createdAt", "updatedAt") VALUES (3, 'De Gea', 'Senior Software Engineer', false, true, '23 March 2021', NULL, 'https://linkedin.com', 'de_gea@gmail.com', '2021-12-21 21:48:17.657+05:30', '2021-12-21 21:48:17.657+05:30');
-INSERT INTO public.employees (id, name, "position", rehire_eligible, voluntary, terminated_date, termination_reason, profile_link, email, "createdAt", "updatedAt") VALUES (4, 'Pogba', 'Senior Software Engineer', false, true, '22 March 2021', NULL, 'https://linkedin.com', 'pogba@gmail.com', '2021-12-21 21:48:17.657+05:30', '2021-12-21 21:48:17.657+05:30');
-INSERT INTO public.employees (id, name, "position", rehire_eligible, voluntary, terminated_date, termination_reason, profile_link, email, "createdAt", "updatedAt") VALUES (5, 'Liam', 'Senior Software Engineer', false, true, '22 March 2021', NULL, 'https://linkedin.com', 'Liam@gmail.com', '2021-12-21 21:48:17.657+05:30', '2021-12-21 21:48:17.657+05:30');
-INSERT INTO public.employees (id, name, "position", rehire_eligible, voluntary, terminated_date, termination_reason, profile_link, email, "createdAt", "updatedAt") VALUES (6, 'Juan', 'Senior Software Engineer', NULL, true, '23 March 2021', NULL, 'https://linkedin.com', 'juan@gmail.com', '2021-12-21 21:48:17.657+05:30', '2021-12-21 21:48:17.657+05:30');
+CREATE TABLE employees (
+  id    serial primary key,
+  name character varying(255) not null,
+  position character varying(255) not null,
+  profile_link character varying(255) not null,
+  terminated_date character varying(255),
+  email character varying(255) not null,
+  rehire_eligible boolean default true,
+  voluntary boolean default false,
+  termination_reason boolean default null,
+  "createdAt" timestamp with time zone DEFAULT CURRENT_TIMESTAMP NOT NULL,
+  "updatedAt" timestamp with time zone DEFAULT CURRENT_TIMESTAMP NOT NULL
+);
+
+INSERT INTO public.employees (id, name, "position", rehire_eligible, voluntary, terminated_date, termination_reason, profile_link, email) VALUES (0, 'Tim', 'Senior Software Engineer', true, true, '23 March 2021', NULL, 'https://linkedin.com', 'tim@gmail.com');
+INSERT INTO public.employees (id, name, "position", rehire_eligible, voluntary, terminated_date, termination_reason, profile_link, email) VALUES (1, 'Bob', 'Senior Software Engineer', true, false, '22 March 2021', NULL, 'https://linkedin.com', 'bob@gmail.com');
+INSERT INTO public.employees (id, name, "position", rehire_eligible, voluntary, terminated_date, termination_reason, profile_link, email) VALUES (2, 'Susan', 'Senior Software Engineer', true, true, '22 March 2021', 'Left because of a new opportunity', 'https://linkedin.com', 'susan@gmail.com');
+INSERT INTO public.employees (id, name, "position", rehire_eligible, voluntary, terminated_date, termination_reason, profile_link, email) VALUES (3, 'De Gea', 'Senior Software Engineer', false, true, '23 March 2021', NULL, 'https://linkedin.com', 'de_gea@gmail.com');
+INSERT INTO public.employees (id, name, "position", rehire_eligible, voluntary, terminated_date, termination_reason, profile_link, email) VALUES (4, 'Pogba', 'Senior Software Engineer', false, true, '22 March 2021', NULL, 'https://linkedin.com', 'pogba@gmail.com');
+INSERT INTO public.employees (id, name, "position", rehire_eligible, voluntary, terminated_date, termination_reason, profile_link, email) VALUES (5, 'Liam', 'Senior Software Engineer', false, true, '22 March 2021', NULL, 'https://linkedin.com', 'Liam@gmail.com');
+INSERT INTO public.employees (id, name, "position", rehire_eligible, voluntary, terminated_date, termination_reason, profile_link, email) VALUES (6, 'Juan', 'Senior Software Engineer', NULL, true, '23 March 2021', NULL, 'https://linkedin.com', 'juan@gmail.com');
 
 
 --
